24e5c348ede891ed8733d29e44d495536604c4ff
MAP01 <maguilera131113@gmail.com>
2026-09-11 05:45:56 -0700

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -173,11 +173,11 @@ function myFunction(typ3) {
 
     // Open or Close Desired Menu
     if (x.style.display === "none") {
-        x.style.display = "block";
-    } else if (x.style.display === "block") {
+        x.style.display = "flex";
+    } else if (x.style.display === "flex") {
         x.style.display = "none";
     } else {
-        x.style.display = "block";
+        x.style.display = "flex";
     }
   }
 
