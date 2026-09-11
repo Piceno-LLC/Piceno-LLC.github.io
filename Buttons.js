1837968dff6d1c30ef8b543a08a03744ffb9f1ed
MAP01 <maguilera131113@gmail.com>
2026-09-11 16:26:08 -0700

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -286,7 +286,7 @@ const themes = {
 const pdfJsSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js";
 const pdfWorkerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js";
 const pdfLibSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.11.0/pdf-lib.min.js";
-*//
+*/
 const pdfJsSrc = "https://www.piceno.dev/PDFjs/pdf.min.js";
 const pdfWorkerSrc = "https://www.piceno.dev/PDFjs/pdf.worker.min.js";
 const pdfLibSrc = "https://www.piceno.dev/PDFjs/pdf-lib.min.js";
