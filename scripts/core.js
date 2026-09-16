fd0d8b08d1e0aeeac8356e55e5375b90f268f215
MAP01 <maguilera131113@gmail.com>
2026-09-16 15:49:36 -0700

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -69,7 +69,7 @@ function innerPage(pageName) {
     const div_ID = pageName + "_DIV";
     if (document.getElementById(div_ID) !== null) {
         let careerTextArea = document.getElementById(div_ID);
-        fetch("./Pages/Projects/" + pageName + "/index.html")
+        fetch("https://www.piceno.dev/mario/pages/projects/" + pageName + "/index.html")
         .then(response => response.text())
         .then(html => {
             careerTextArea.innerHTML = html;
