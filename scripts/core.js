923994323ff750ee365fc41582717f6838c4ff87
MAP01 <maguilera131113@gmail.com>
2026-09-16 14:37:21 -0700

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -187,7 +187,7 @@ function myFunction(typ3) {
     if (x.style.display === "none") {
         x.style.display = "flex";
         window.scrollTo(scrollX, scrollY);
-    } if (x.style.display === "flex") {
+    } else if (x.style.display === "flex") {
          x.style.display = "none";
     } else {
         x.style.display = "none";
