036155501cb1ae4ec94d42a797c984d074fcc7c6
MAP01 <maguilera131113@gmail.com>
2026-09-20 12:06:24 -0700

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -42,7 +42,6 @@ function openMobileNav() {
         x.style.display = "flex";
     } else if (x.style.display === "flex") {
          x.style.display = "none";
-         x.style.display = '';
     } else {
         x.style.display = "flex";
     }
